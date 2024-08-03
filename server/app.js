4b266d2fe2708d5bb0226b81ac77c5725cd3e061
homeg <homegor14@gmail.com>
2024-08-03 07:46:36 +0700

    update api

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.options("*", cors());
+app.use(cors());
 
 app.use("/api", routes);
 const PORT = process.env.PORT || config.get("port") || 8080;
